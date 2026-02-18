0325346c52464f39b722e3a66e61840683d8c573
Wateir <contact@wateir.fr>
2026-02-18 14:28:24 +0100

    Remove file

--- Project/Complet/standard.c
+++
@@ -1,8 +0,0 @@
-#include "standard.h"
-
-int ReStrcmp(const char ch1[],const char ch2[],const long taillech1){
-    for(int i=0;i<taillech1;i++){
-        if(ch1[i] != ch2[i]) return 1;
-    }
-    return 0;
-}
